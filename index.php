36e65085e07ad9d263af37264b41b803fd65ff96
Aissata26 <aissata.diallo@groupe-esigelec.org>
2025-10-06 09:43:14 +0200

    Création de la page d'accueil

---
+++ index.php
@@ -0,0 +1,10 @@
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Aissata</title>
+    </head>
+    <body>
+        <p> Aissata La femme de ma vie</p>
+    </body>
+</html>
