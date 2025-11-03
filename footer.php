55e56bbe979a5f84388819f019fc4ab112ec34d4
Zoulkif21 <zoul.saibou@groupe-esigelec.org>
2025-11-03 01:22:51 +0100

    dossier

---
+++ footer.php
@@ -0,0 +1,45 @@
+<?php
+include_once'header.php';
+?>
+<footer class="background text-light py-4 " >
+    <div class="container justify-content-center align-items-center text-center ">
+        <div class="row p-4">
+            <div class="col-12 col-md-3">
+                <a class="text-light fw-bold fs-2" href="#">MOVE<span class="titre-color">UP</span></a>
+                <p class="fs-6 text-secondary">Trouvez les meilleurs déménageurs près de chez vous en toute confiance.</p>
+            </div>
+            <div class="col-12 col-md-3">
+                <h4>Services</h4>
+                <a class="fs-6 text-secondary d-block" href="#">Créer une annonce</a>
+                <a class="fs-6 text-secondary d-inlign-block" href="#">Devenir déménageur</a>
+            </div>
+            <div class="col-12 col-md-3">
+                <h4>A propos </h4>
+                <a class="fs-6 text-secondary d-block" href="#">Qui sommes-nous</a>
+                <a class="fs-6 text-secondary d-inlign-block" href="#">Comment ça marche</a>
+            </div>
+            <div class="col-12 col-md-3">
+                <h4>Contact</h4>
+                <a class="fs-6 text-secondary d-block" href="#">Support client</a>
+                <a class="fs-6 text-secondary d-block" href="#">Contactez nous</a>
+            </div>
+        </div>
+        <div class="row align-items-center border-top border-secondary justify-content-center text-center p-4">
+            <div class="col-md-3 col-12 ">
+                <a href="#" ><img src="photo/photoe.png" alt="twitter"></a>
+                <a href="#" ><img src="photo/photog.png" alt="intagram"></a>
+                <a href="#" ><img src="photo/photoh.png" alt="facebook"></a>
+            </div>
+            <div class="col-md-6 col-12">
+                <a href="#" class="text-secondary">Mentions légales </a>
+                <span> . </span>
+                <a href="#" class="text-secondary">Politique de confidentialité</a>
+                <span> . </span>
+                <a href="#" class="text-secondary">Conditions d'utilisation</a>
+            </div> 
+            <div class="col-md-3 col-12">
+                <p class="text-secondary">2025 MoveUp.Tous droits réservés.</p>
+            </div>
+        </div>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -1,10 +1,130 @@
-<!Doctype html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Aissata</title>
-    </head>
-    <body>
-        <p> Aissata La femme de ma vie</p>
-    </body>
-</html>
+<?php 
+include_once 'header.php';
+?>
+<body>
+    <header>
+        <?php
+        include_once 'nav.php'
+        ?>
+    </header>
+    <main>
+        <section class="background text-light">
+            <div class="container">
+                <div class="row py-5">
+                    <div class="col-md-6 py-2">        
+                        <h1 class="fs-1 fw-bolder">DÉMÉNAGEZ EN TOUTE CONFIANCE</h1>
+                        <P class="fs-6">Trouvez les meilleurs déménageurs près de chez vous.</p>
+                        <div class="d-flex gap-3 flex-nowrap">
+                            <a href="#" class="btn btn-outline-primary no-outline rounded-1 text-light btn-lg ">Créer une demande</a>
+                            <a href="#" class="btn btn-outline-success no-outline rounded-1 text-light btn-lg px-5">Voir les offres</a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 py-4">
+                        <img src="photo/photoc.png" alt="photo de camion" class="img-fluid">
+                    </div>
+                </div>
+            </dib>
+        </section>
+        <section class="background p-4">
+            <div class="container text-light bg">
+                <div class="row ">
+                    <h2 class="text-center my-4">Comment ça mache</h2>
+                </div>
+                <div class="row ">
+                    <div class="col-12 col-md-4">
+                        <div class="text-center my-2">
+                            <img src="photo/photoa.png" alt="message">
+                        </div>
+                        <p class="text-center">1.créez une annonce</p>
+                        <p class="text-center">Définissez les détails de votre déménagement.</p>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <div class="text-center my-2">
+                            <img src="photo/photob.png" alt="proposition">
+                        </div>
+                        <p class="text-center">2.Recevez des propositions</p>
+                        <p class="text-center">Comparez les offres des déménageurs.</p>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <div class="text-center my-2">
+                            <img src="photo/photof.png" alt="validation">
+                        </div>
+                        <p class="text-center">3.Choisissez vos déménageurs</p>
+                        <p class="text-center">Sélectionnez des pesonnes de confiance.<p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="background py-4">
+            <div class="container text-light ">
+                <div class="row">
+                    <h2 class="text-center py-4">ANNONCES RÉCENTES</h2>
+                </div>
+                <div class="row text-light">
+                    <div class="col-12 col-lg-4">
+                        <div class="card position-relative bg my-4">
+                            <div class="card-body">
+                                <div class="text-end">
+                                    <p class="d-inline-block m-3 px-2 py-1 border border-3 border-primary bg-primary  transparent-bleu">Moyen T2/T3</p>
+                                </div>
+                                <div class="row">
+                                    <h5 class="card-title fw-bolder fs-4">Paris-Lyon</h5>
+                                    <p class="card-text">Date: 15 octobre 2025</p>
+                                    <p class="card-text">Déménagement d'un T2 au 3e étage avec ascenceur</p>
+                                    <div class="d-flex justify-content-between align-items-center border-top">
+                                        <p class="card-text">Publié le 20/09/2025</p>
+                                        <a href="#" class="btn btn-success d-inline-block m-3 px-1 border-3 border-success bg-success transparent-green">Voir les détails</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-4">
+                        <div class="card position-relative bg my-4">
+                            <div class="card-body">
+                                <div class="text-end">
+                                    <p class="d-inline-block m-3 px-2 py-1 border border-3 border-primary bg-primary  transparent-bleu">Moyen T2/T3</p>
+                                </div>
+                                <div class="row">
+                                    <h5 class="card-title fw-bolder fs-4">Marseille - Nice</h5>
+                                    <p class="card-text">Date: 20 octobre 2025</p>
+                                    <p class="card-text">studio au 2e étage sans ascenceur</p>
+                                    <div class="d-flex justify-content-between align-items-center border-top">
+                                        <p class="card-text">Publié le 21/09/2025</p>
+                                        <a href="#" class="btn btn-success d-inline-block m-3 px-1 border-3 border-success bg-success transparent-green">Voir les détails</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-4">
+                        <div class="card position-relative bg my-4">
+                            <div class="card-body">
+                                <div class="text-end">
+                                    <p class="d-inline-block m-3 px-2 py-1 border border-3 border-primary bg-primary  transparent-bleu">Moyen T2/T3</p>
+                                </div>
+                                <div class="row">
+                                    <h5 class="card-title fw-bolder fs-4">Bordeaux - Toulouse</h5>
+                                    <p class="card-text">Date: 1 octobre 2025</p>
+                                    <p class="card-text">Maison 4 pièces avec beaucoup</p>
+                                    <div class="d-flex justify-content-between align-items-center border-top">
+                                        <p class="card-text">Publié le 21/09/2025</p>
+                                        <a href="#" class="btn btn-success d-inline-block m-3 px-1 border-3 border-success bg-success transparent-green">Voir les détails</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-auto">
+                        <a href="#" class="btn btn-primary my-4 py-2">voire toutes les annonces</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php include_once 'footer.php'?>
+</body>
+                    
+        
