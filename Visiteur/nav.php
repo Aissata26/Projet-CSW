c06161735cd8fec814d94a0b1ddea07ffbe61b6c
Zoulkif21 <zoul.saibou@groupe-esigelec.org>
2025-11-11 18:12:43 +0100

    page de connexion

--- Visiteur/nav.php
+++ Visiteur/nav.php
@@ -30,6 +30,37 @@ include_once '../fonction.php';
   </div>
 </nav>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<?php connexion();?>
+<div class="modal fade  " id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content background--blanc-crem border-0  text-center">
+                    <div class="modal-header border-0">    
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="row text-center">
+                        <h1 class="text-color-bleufoncé fs-2">Connexion</h1>
+                    </div>
+                    <div class="row text-center">
+                        <p class="text-couleur-blanc-creme">Connectez-vous à votre compte</p>
+                    </div>
+                    <div class="modal-body">
+                        <form class="justify-content-center"  method="POST" action="../fonction.php">
+                            <div class="mb-3">
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" placeholder="Votre@email.com">
+                            </div>
+                            <div class="mb-3">
+                                <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="*********">
+                            </div>
+                            <div class="text-end">
+                                <a href="#" >Mot de passe oublié?</a>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Connexion</button></div>
+                        </form>
+                        <div class="text-center text-light border-top p-3">
+                            <p class="">Pas encore de compte ? <a href="#">Créer un compte</p>
+                        </div>
+                    </div>  
+                </div>
+            </div>
+        </div>
 </body>
 
--- fonction.php
+++ fonction.php
@@ -1,19 +1,67 @@
 <?php 
-$postData = $_POST;
-function connexion_base_de_donnee()
-{
     try
     {
-        $pdo= new PDO("mysql:host=localhost;dbname=tableaux","root","");
+        $pdo= new PDO("mysql:host=localhost;dbname=bdd_7_13","root","");
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
     }
     catch(Exception $e)
     {
         die('Erreur : '.$e->getMessage());
     }
-}
-connexion_base_de_donnee();
+function get_utilisateur()
+{
+    global $pdo;
+    $utilisateurStatement = $pdo->prepare('select * from utilisateur');
+    $utilisateurStatement->execute();
+    $utilisateurs=$utilisateurStatement->fetchAll();
+    return $utilisateurs;
 
-function connexion ()
+}
+
+    $utilisateurs=get_utilisateur();
+   if ( isset($_POST['email']) && isset($_POST['password']) )
+   {
+    if (!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+    {
+        echo('Il faut un email valide');
+    }
+    else
+    {
+      if(empty($_POST['password']) || trim($_POST['password']) === '')
+      {
+        echo('Il faut un mot de passe valide');
+      }
+      else
+      {
+          foreach($utilisateurs as $utilisateur)
+         {
+            if($utilisateur['email'] === $_POST['email'])
+            {
+                if($utilisateur['motdepasse'] === $_POST['password'])
+                {
+                    header("Location: Client/client.php");
+                    exit();
+                }
+                else
+                {
+                    $message1="mot de passe invalide";
+                }
+            }
+            else
+            {
+                $message="Email invalide";
+
+            }
+         }
+      }
+    }
+}
+      
+  
+     
+
+
+/*function connexion ()
 {
        echo ' <div class="modal fade  " id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog">
@@ -28,17 +76,19 @@ function connexion ()
                         <p class="text-couleur-blanc-creme">Connectez-vous à votre compte</p>
                     </div>
                     <div class="modal-body">
-                        <form class="justify-content-center">
+                        <form class="justify-content-center"  method="POST" action="fonction.php">
                             <div class="mb-3">
+                                <p><?php echo($message);?></p>
                                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" placeholder="Votre@email.com">
                             </div>
                             <div class="mb-3">
+                            <p><?php echo($message1);?></p>
                                 <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="*********">
                             </div>
                             <div class="text-end">
                                 <a href="#" >Mot de passe oublié?</a>
                             </div>
-                            <button type="bouton" class="btn btn-primary">Connexion</button></div>
+                            <button type="submit" class="btn btn-primary">Connexion</button></div>
                         </form>
                         <div class="text-center text-light border-top p-3">
                             <p class="">Pas encore de compte ? <a href="#">Créer un compte</p>
@@ -47,28 +97,7 @@ function connexion ()
                 </div>
             </div>
         </div>';
-}
+}*/
 
-function form_connexion()
-{
-   if ( !isset($postData['email']) && !isset($postData['password']) )
-   {
-    if (!filter_var($postData['email'],FILTE_VALIDATE_EMAIL))
-    {
-        echo('Il faut un email valide');
-    }
-    else
-    {
-      if(empty($postData['password']) || trim($postData['password']) === '')
-      {
-        echo('Il faut un mot de passe valide');
-      }
-      else
-      {
-         
-      }
-    }
-   }
-}
 
 ?>
