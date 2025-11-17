e77139fdab83918bb9e293dd3d3d71fd7e745afb
Zoulkif21 <zoul.saibou@groupe-esigelec.org>
2025-11-17 02:20:32 +0100

    mise a jour base de donné

--- tableaux.sql
+++ tableaux.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : ven. 17 oct. 2025 à 18:28
+-- Généré le : lun. 17 nov. 2025 à 02:18
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `tableaux`
+-- Base de données : `bdd_7_13`
 --
 
 -- --------------------------------------------------------
@@ -76,11 +76,23 @@ CREATE TABLE `proposition` (
 CREATE TABLE `utilisateur` (
   `id` int(11) NOT NULL,
   `nom` varchar(256) NOT NULL,
+  `prenom` varchar(250) NOT NULL,
   `email` varchar(256) NOT NULL,
   `motdepasse` varchar(256) NOT NULL,
   `role` varchar(256) NOT NULL,
-  `telephone` date NOT NULL
+  `telephone` int(11) NOT NULL,
+  `dateanniverssaire` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='table des utilisateur';
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `email`, `motdepasse`, `role`, `telephone`, `dateanniverssaire`) VALUES
+(1, 'SAIBOU', 'Zoul', 'saibouzoul@gmail.com', 'Zoul123', 'Client', 780564240, '2001-01-01'),
+(2, 'LANTONKPODE', 'Steve', 'lantonkpodesteve@gmail.com', 'Steve1234', 'Déménageur', 700000001, '0000-00-00'),
+(3, 'GBETOKPE', 'Rolland', 'gbetokperolland@gmail.com', 'rolland123', 'Client', 700000002, '2002-01-02'),
+(6, 'ZANNOU', 'Martin', 'zannoumartin@gmail.com', '$2y$10$E7OiPkFV27ZtcVNPhBYToOebUWHDpaQ14xZYiDz5ahVnWR/5WJVai', 'Déménageur', 700000002, '2001-01-04');
 
 --
 -- Index pour les tables déchargées
@@ -113,6 +125,16 @@ ALTER TABLE `proposition`
 --
 ALTER TABLE `utilisateur`
   ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Contraintes pour les tables déchargées
